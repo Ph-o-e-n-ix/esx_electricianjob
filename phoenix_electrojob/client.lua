de4dac2e09fff66545a539d7fb0c21cc5b513f37
Phoenix Studios <119653707+Ph-o-e-n-ix@users.noreply.github.com>
2023-10-06 23:58:28 +0200

    Update client.lua

--- phoenix_electrojob/client.lua
+++ phoenix_electrojob/client.lua
@@ -31,6 +31,8 @@ busy3 = false
 step5 = false
 blipactive2 = false
 
+
+local inminigame = false
 Citizen.CreateThread(function()
 	while true do 
 		Citizen.Wait(0)
@@ -50,189 +52,210 @@ Citizen.CreateThread(function()
 				end
 				if IsControlJustReleased(0, 38) then 
 					if not busy then 
-						busy = true
-						blipactive = false
-						Config.MSG(Translation[Config.Locale]['started_mission'])
-						local car  = GetHashKey(v.vehiclename)
-						RequestModel(car)	
-						while not HasModelLoaded(car) do 
-							RequestModel(car)
-							Citizen.Wait(50)
-						end 
-						vehicle = CreateVehicle(car, v.spawncoords.x, v.spawncoords.y, v.spawncoords.z, v.carheading, true, false)
-						SetVehicleLivery(vehicle, 10)
-						SetVehicleDirtLevel(vehicle, 0.0)
-						--[[local getFuel = exports['LegacyFuel']:GetFuel(vehicle)
-						local fuelLevel = getFuel 
-						exports['LegacyFuel']:SetFuel(vehicle, 100)]]
-						--SetPedIntoVehicle(player, vehicle, -1) --uncomment to Spawn the Player into the Vehicle
-						SetEntityAsNoLongerNeeded(vehicle)
-						SetVehicleNumberPlateText(vehicle, Config.CarPlateName)
-						SetEntityAsMissionEntity(vehicle, false, false)
-						missionvehicle = vehicle
-						Citizen.CreateThread(function()
-							while busy do
-								Citizen.Wait(0)
-								missionvehiclecoords = GetEntityCoords(missionvehicle)
-								local playerPed = PlayerPedId()
-								playercoords2 = GetEntityCoords(playerPed)
-								if not blipactive2 then
-									blipactive2 = true
-									fahrzeugblip = AddBlipForEntity(missionvehicle)
-									SetBlipSprite(fahrzeugblip, 227)
-									SetBlipScale(fahrzeugblip, 0.9)
-									SetBlipColour(fahrzeugblip, 3)
-									BeginTextCommandSetBlipName("STRING")
-									AddTextComponentString(Translation[Config.Locale]['car_blipname'])
-									EndTextCommandSetBlipName(fahrzeugblip)
-								end
-								if IsPedInVehicle(playerPed, missionvehicle, false) then 
-									RemoveBlip(fahrzeugblip)
-									blipactive2 = false
-								end
-								if not step1 then 
-									step1 = true
-									results = Config.MissionCoords[math.random(#Config.MissionCoords)]
-									Citizen.Wait(5)
-								end
-								if not blipactive then 
-									blipactive = true
-									missionblip = AddBlipForCoord(results.x, results.y, results.z)
-									SetBlipSprite(missionblip, 1)
-									SetBlipScale(missionblip, 0.9)
-									SetBlipColour(missionblip, 3)
-									BeginTextCommandSetBlipName("STRING")
-									AddTextComponentString(Translation[Config.Locale]['target_blip'])
-									EndTextCommandSetBlipName(missionblip)
-									SetBlipRoute(missionblip, true)
-								end
-								if step1 then 
-									local playerPed = PlayerPedId()
-									local vehicle = missionvehicle
-									local trunkpos = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "boot"))
-									local playerCoords = GetEntityCoords(PlayerPedId())
-									local Distance3 = Vdist(playerCoords, trunkpos)
-									local dst = GetDistanceBetweenCoords(playercoords2, results.x, results.y, results.z)
-									if dst < 15 then
-										DrawMarker(21, results.x, results.y, results.z , 0, 0, 0, 0, 0, 0, 0.5,0.5,0.6, 32,186,252, 150, 0, 0, 2, 1, 0, 0, 0)
-										if Distance3 < 9 and not IsPedInAnyVehicle(PlayerPedId(), false) then 
-											if not busy3 then 
-												DrawText3D(trunkpos.x , trunkpos.y, trunkpos.z , Translation[Config.Locale]['take_toolkit'])
-												step4 = true
-											end
-											if Distance3 < 1 then 
-												if IsControlJustReleased(0, 38) then 
-													busy3 = true
-													SetVehicleDoorOpen(missionvehicle, 5, false, false)
-													Citizen.Wait(500)
-													TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
-													Citizen.Wait(2500)
-													ClearPedTasksImmediately(PlayerPedId())
-													toolkitprop = CreateObject(GetHashKey("imp_prop_tool_box_01a"), 0, 0, 0, true, true, true)
-													local prophand = AttachEntityToEntity(toolkitprop, playerPed, GetPedBoneIndex(playerPed, 28422), 0.3700, 0.0200, 0, 90.00, 0, -90.00, true, true, true, false, 1, true)
-													RequestAnimDict('move_weapon@jerrycan@generic')
-													while not HasAnimDictLoaded('move_weapon@jerrycan@generic') do 
-														Citizen.Wait(50)
+						ESX.TriggerServerCallback('phoenix_electric:hasjob', function(hasjob)
+							if hasjob then
+								busy = true
+								blipactive = false
+								Config.MSG(Translation[Config.Locale]['started_mission'])
+								local car  = GetHashKey(v.vehiclename)
+								RequestModel(car)	
+								while not HasModelLoaded(car) do 
+									RequestModel(car)
+									Citizen.Wait(50)
+								end 
+								vehicle = CreateVehicle(car, v.spawncoords.x, v.spawncoords.y, v.spawncoords.z, v.carheading, true, false)
+								SetVehicleLivery(vehicle, 10)
+								SetVehicleDirtLevel(vehicle, 0.0)
+								--[[local getFuel = exports['LegacyFuel']:GetFuel(vehicle)
+								local fuelLevel = getFuel 
+								exports['LegacyFuel']:SetFuel(vehicle, 100)]]
+								--SetPedIntoVehicle(player, vehicle, -1) --uncomment to Spawn the Player into the Vehicle
+								SetEntityAsNoLongerNeeded(vehicle)
+								SetVehicleNumberPlateText(vehicle, Config.CarPlateName)
+								SetEntityAsMissionEntity(vehicle, false, false)
+								missionvehicle = vehicle
+								Citizen.CreateThread(function()
+									while busy do
+										Citizen.Wait(0)
+										missionvehiclecoords = GetEntityCoords(missionvehicle)
+										local playerPed = PlayerPedId()
+										playercoords2 = GetEntityCoords(playerPed)
+										if not blipactive2 then
+											blipactive2 = true
+											fahrzeugblip = AddBlipForEntity(missionvehicle)
+											SetBlipSprite(fahrzeugblip, 227)
+											SetBlipScale(fahrzeugblip, 0.9)
+											SetBlipColour(fahrzeugblip, 3)
+											BeginTextCommandSetBlipName("STRING")
+											AddTextComponentString(Translation[Config.Locale]['car_blipname'])
+											EndTextCommandSetBlipName(fahrzeugblip)
+										end
+										if IsPedInVehicle(playerPed, missionvehicle, false) then 
+											RemoveBlip(fahrzeugblip)
+											blipactive2 = false
+										end
+										if not step1 then 
+											step1 = true
+											results = Config.MissionCoords[math.random(#Config.MissionCoords)]
+											Citizen.Wait(5)
+										end
+										if not blipactive then 
+											blipactive = true
+											missionblip = AddBlipForCoord(results.x, results.y, results.z)
+											SetBlipSprite(missionblip, 1)
+											SetBlipScale(missionblip, 0.9)
+											SetBlipColour(missionblip, 3)
+											BeginTextCommandSetBlipName("STRING")
+											AddTextComponentString(Translation[Config.Locale]['target_blip'])
+											EndTextCommandSetBlipName(missionblip)
+											SetBlipRoute(missionblip, true)
+										end
+										if step1 then 
+											local playerPed = PlayerPedId()
+											local vehicle = missionvehicle
+											local trunkpos = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "boot"))
+											local playerCoords = GetEntityCoords(PlayerPedId())
+											local Distance3 = Vdist(playerCoords, trunkpos)
+											local dst = GetDistanceBetweenCoords(playercoords2, results.x, results.y, results.z)
+											if dst < 15 then
+												DrawMarker(21, results.x, results.y, results.z , 0, 0, 0, 0, 0, 0, 0.5,0.5,0.6, 32,186,252, 150, 0, 0, 2, 1, 0, 0, 0)
+												if Distance3 < 9 and not IsPedInAnyVehicle(PlayerPedId(), false) then 
+													if not busy3 then 
+														DrawText3D(trunkpos.x , trunkpos.y, trunkpos.z , Translation[Config.Locale]['take_toolkit'])
+														step4 = true
 													end
-													TaskPlayAnim(PlayerPedId(), 'move_weapon@jerrycan@generic', 'idle', 8.0, 8.0, -1, 51, 0, 0, 0, 0)
-													Citizen.Wait(1000)
-													SetVehicleDoorShut(vehicle, 5, false)
-													step5 = true 
-												end
-											end
-										end 
-										if step4 then 
-											if dst < 1.5 then 
-												if not shownotify2 then
-													shownotify(Translation[Config.Locale]['press_to_start_fixing'])
-												end
-												if IsControlJustReleased(0, Config.StartKey) then 
-													if step5 then
-														local distance2 = Vdist(missionvehiclecoords, results.x, results.y, results.z)
-														if distance2 < 15 then
-															shownotify2 = true
-															FreezeEntityPosition(PlayerPedId(), true)
-															SetEntityCoords(PlayerPedId(),results.x, results.y, results.z -1.0, true, false, false, false)
-															SetEntityHeading(playerPed, results.h)
-															DeleteEntity(toolkitprop)
-															if Config.UseMiniGame then
-																local luck = math.random(100)
-																if luck > 50 then 
-																	TriggerEvent("Mx::StartMinigameElectricCircuit", '50%', '100%', '0.8', '30vmin', '1.ogg', function(mist)
-																		if Callback == mist then
-																			--Citizen.Wait(5000)
-																			TaskStartScenarioInPlace(playerPed, 'WORLD_HUMAN_WELDING', 0, true)
-																			Citizen.Wait(Config.Waittime * 1000)
-																			ClearPedTasks(playerPed)
-																			Config.MSG(Translation[Config.Locale]['successfully_repaired'])
-																			RemoveBlip(missionblip)
-																			step1 = false
-																			busy3 = false
-																			step5 = false
-																			blipactive = false
-																			FreezeEntityPosition(PlayerPedId(), false)
-																			Citizen.Wait(1500)
-																			TriggerServerEvent("electric:getmoney")
-																			showPictureNotification(Translation[Config.Locale]['img'], Translation[Config.Locale]['message'], Translation[Config.Locale]['title'], Translation[Config.Locale]['subtitle'])
-																			shownotify2 = false
-																		else
-																			Config.MSG(Translation[Config.Locale]['failed_minigame'])
+													if Distance3 < 1 then 
+														if IsControlJustReleased(0, 38) then 
+															busy3 = true
+															SetVehicleDoorOpen(missionvehicle, 5, false, false)
+															Citizen.Wait(500)
+															TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
+															Citizen.Wait(2500)
+															ClearPedTasksImmediately(PlayerPedId())
+															toolkitprop = CreateObject(GetHashKey("imp_prop_tool_box_01a"), 0, 0, 0, true, true, true)
+															local prophand = AttachEntityToEntity(toolkitprop, playerPed, GetPedBoneIndex(playerPed, 28422), 0.3700, 0.0200, 0, 90.00, 0, -90.00, true, true, true, false, 1, true)
+															RequestAnimDict('move_weapon@jerrycan@generic')
+															while not HasAnimDictLoaded('move_weapon@jerrycan@generic') do 
+																Citizen.Wait(50)
+															end
+															TaskPlayAnim(PlayerPedId(), 'move_weapon@jerrycan@generic', 'idle', 8.0, 8.0, -1, 51, 0, 0, 0, 0)
+															Citizen.Wait(1000)
+															SetVehicleDoorShut(vehicle, 5, false)
+															step5 = true 
+														end
+													end
+												end 
+												if step4 then 
+													if dst < 1.5 then 
+														if not shownotify2 then
+															shownotify(Translation[Config.Locale]['press_to_start_fixing'])
+														end
+														if IsControlJustReleased(0, Config.StartKey) then 
+															if step5 then
+																local distance2 = Vdist(missionvehiclecoords, results.x, results.y, results.z)
+																if distance2 < 15 then
+																	shownotify2 = true
+																	FreezeEntityPosition(PlayerPedId(), true)
+																	SetEntityCoords(PlayerPedId(),results.x, results.y, results.z -1.0, true, false, false, false)
+																	SetEntityHeading(playerPed, results.h)
+																	DeleteEntity(toolkitprop)
+																	if Config.UseMiniGame and not inminigame then
+																		inminigame = true
+																		local luck = math.random(100)
+																		if luck > 50 then 
+																			if inminigame then
+																				TriggerEvent("Mx::StartMinigameElectricCircuit", '50%', '100%', '0.8', '30vmin', '1.ogg', function(mist)
+																					if Callback == mist then
+																						if inminigame then
+																							--Citizen.Wait(5000)
+																							TaskStartScenarioInPlace(playerPed, 'WORLD_HUMAN_WELDING', 0, true)
+																							Citizen.Wait(Config.Waittime * 1000)
+																							ClearPedTasks(playerPed)
+																							Config.MSG(Translation[Config.Locale]['successfully_repaired'])
+																							RemoveBlip(missionblip)
+																							step1 = false
+																							busy3 = false
+																							step5 = false
+																							blipactive = false
+																							FreezeEntityPosition(PlayerPedId(), false)
+																							Citizen.Wait(1500)
+																							TriggerServerEvent("electric:getmoney")
+																							showPictureNotification(Translation[Config.Locale]['img'], Translation[Config.Locale]['message'], Translation[Config.Locale]['title'], Translation[Config.Locale]['subtitle'])
+																							shownotify2 = false
+																							inminigame = false
+																						end
+																					else
+																						Config.MSG(Translation[Config.Locale]['failed_minigame'])
+																						FreezeEntityPosition(PlayerPedId(), false)
+																						inminigame = false
+																					end
+																				end)
+																			end
+																		else 
+																			if inminigame then
+																				TriggerEvent("datacrack:start", 3, function(output)
+																					if output == true then
+																						if inminigame then
+																							DeleteEntity(toolkitprop)
+																							Citizen.Wait(500)
+																							TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
+																							Citizen.Wait(Config.Waittime * 1000)
+																							ClearPedTasks(playerPed)
+																							Config.MSG(Translation[Config.Locale]['successfully_repaired'])
+																							RemoveBlip(missionblip)
+																							step1 = false
+																							busy3 = false
+																							step5 = false
+																							blipactive = false
+																							FreezeEntityPosition(PlayerPedId(), false)
+																							Citizen.Wait(1500)
+																							TriggerServerEvent("electric:getmoney")
+																							showPictureNotification(Translation[Config.Locale]['img'], Translation[Config.Locale]['message'], Translation[Config.Locale]['title'], Translation[Config.Locale]['subtitle'])
+																							shownotify2 = false
+																							inminigame = false
+																						end
+																					else
+																						Config.MSG(Translation[Config.Locale]['failed_minigame'])
+																						FreezeEntityPosition(PlayerPedId(), false)
+																						inminigame = false
+																					end
+																				end)
+																			end
 																		end
-																	end)
+																	else 
+																		Citizen.Wait(500)
+																		TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
+																		Citizen.Wait(Config.Waittime * 1000)
+																		ClearPedTasks(playerPed)
+																		Config.MSG(Translation[Config.Locale]['successfully_repaired'])
+																		RemoveBlip(missionblip)
+																		step1 = false
+																		busy3 = false
+																		step5 = false
+																		blipactive = false
+																		FreezeEntityPosition(PlayerPedId(), false)
+																		Citizen.Wait(1500)
+																		TriggerServerEvent("electric:getmoney")
+																		showPictureNotification(Translation[Config.Locale]['img'], Translation[Config.Locale]['message'], Translation[Config.Locale]['title'], Translation[Config.Locale]['subtitle'])
+																		shownotify2 = false
+																	end
+
 																else 
-																	TriggerEvent("datacrack:start", 3, function(output)
-																		if output == true then
-																			DeleteEntity(toolkitprop)
-																			Citizen.Wait(500)
-																			TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
-																			Citizen.Wait(Config.Waittime * 1000)
-																			ClearPedTasks(playerPed)
-																			Config.MSG(Translation[Config.Locale]['successfully_repaired'])
-																			RemoveBlip(missionblip)
-																			step1 = false
-																			busy3 = false
-																			step5 = false
-																			blipactive = false
-																			FreezeEntityPosition(PlayerPedId(), false)
-																			Citizen.Wait(1500)
-																			TriggerServerEvent("electric:getmoney")
-																			showPictureNotification(Translation[Config.Locale]['img'], Translation[Config.Locale]['message'], Translation[Config.Locale]['title'], Translation[Config.Locale]['subtitle'])
-																			shownotify2 = false
-																		else
-																			Config.MSG(Translation[Config.Locale]['failed_minigame'])
-																		end
-																	end)
+																	Config.MSG(Translation[Config.Locale]['vehicle_not_in_range'])
 																end
 															else 
-																Citizen.Wait(500)
-																TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
-																Citizen.Wait(Config.Waittime * 1000)
-																ClearPedTasks(playerPed)
-																Config.MSG(Translation[Config.Locale]['successfully_repaired'])
-																RemoveBlip(missionblip)
-																step1 = false
-																busy3 = false
-																step5 = false
-																blipactive = false
-																FreezeEntityPosition(PlayerPedId(), false)
-																Citizen.Wait(1500)
-																TriggerServerEvent("electric:getmoney")
-																showPictureNotification(Translation[Config.Locale]['img'], Translation[Config.Locale]['message'], Translation[Config.Locale]['title'], Translation[Config.Locale]['subtitle'])
-																shownotify2 = false
+																Config.MSG(Translation[Config.Locale]['toolkit_needed'])
 															end
-
-														else 
-															Config.MSG(Translation[Config.Locale]['vehicle_not_in_range'])
 														end
-													else 
-														Config.MSG(Translation[Config.Locale]['toolkit_needed'])
-													end
+													end						
 												end
-											end						
+											end
 										end
-									end
-								end
-							end 
+									end 
+								end)
+							else 
+								Config.MSG(Translation[Config.Locale]['dont_have_job'])
+							end
 						end)
 					else 
 						Config.MSG(Translation[Config.Locale]['already_startet_mission'])
